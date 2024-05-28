0c238337f47974430eb946b0432b8d4fc1a5c4ef
IceCream <www@withdata.net>
2024-05-28 23:52:34 +0800

    添加 OnebotWebSocketClient

---
+++ src/main/kotlin/com/icecreamqaq/yuq/onebot/connect/OnebotWebSocketClient.kt
@@ -0,0 +1,16 @@
+package com.icecreamqaq.yuq.onebot.connect
+
+import java.io.Closeable
+
+class OnebotWebSocketClient: Closeable {
+
+    fun connect(url:String){
+        TODO("Not yet implemented")
+    }
+
+    override fun close() {
+        TODO("Not yet implemented")
+    }
+
+
+}
