6e0758adeaf28258531f1a564bfb4d7e6e12c0bf
IceCream <www@withdata.net>
2024-05-31 00:54:45 +0800

    完成基本的发信功能

--- src/main/kotlin/com/icecreamqaq/yuq/onebot/connect/OnebotWebSocketClient.kt
+++ src/main/kotlin/com/icecreamqaq/yuq/onebot/connect/OnebotWebSocketClient.kt
@@ -1,12 +1,17 @@
 package com.icecreamqaq.yuq.onebot.connect
 
+import com.IceCreamQAQ.Yu.toJSONString
+import com.alibaba.fastjson.JSONObject
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import java.io.Closeable
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
 
 class OnebotWebSocketClient(
     private val url: String,
@@ -19,7 +24,29 @@ class OnebotWebSocketClient(
         .build()
     lateinit var wsConnect: WebSocket
 
+    val msgSeq = AtomicInteger()
+    val messageMap = ConcurrentHashMap<Int, CompletableDeferred<JSONObject>>()
 
+    data class SendMessage(
+        val echo: String,
+        val action: String,
+        val params: Map<String, Any>
+    )
+
+    suspend fun sendAction(action: String, params: Map<String, Any>): JSONObject {
+        val echo = msgSeq.incrementAndGet()
+        val msg = SendMessage(echo = echo.toString(), action = action, params = params).toJSONString()
+        val msgWait = CompletableDeferred<JSONObject>()
+        messageMap[echo] = msgWait
+        return try {
+            withTimeout(5000) {
+                wsConnect.send(msg)
+                return@withTimeout msgWait.await()
+            }
+        } finally {
+            messageMap.remove(echo)
+        }
+    }
 
     suspend fun connect() {
         isClosed = false
@@ -37,14 +64,22 @@ class OnebotWebSocketClient(
             }
 
             override fun onMessage(webSocket: WebSocket, text: String) {
-                super.onMessage(webSocket, text)
+                val json = JSONObject.parseObject(text)
+
+                val echo = json.getInteger("echo")
+                if (echo != null)
+                    messageMap[echo]?.let {
+                        if (json.getIntValue("retcode") != 0)
+                            it.completeExceptionally(RuntimeException("OneBot Error! status: ${json.getString("status")}"))
+                        else it.complete(json.getJSONObject("data"))
+                    }
             }
 
             override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
                 runBlocking { reConnect() }
             }
         }
-        wsConnect = client.newWebSocket(Request.Builder().url("ws://$url").build(), handler)
+        wsConnect = client.newWebSocket(Request.Builder().url(url).build(), handler)
         connectWait.await()
     }
 
